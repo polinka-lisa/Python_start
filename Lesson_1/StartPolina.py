2238fde702b8977eb9ac3e1de6173b0f85f941a9
polinka-lisa <pmakiyova@ozon.ru>
2021-07-25 19:15:38 +0300

    probe

---
+++ Lesson_1/StartPolina.py
@@ -0,0 +1,19 @@
+import this # Комментарий
+'''
+многострочный
+комментарий
+'''
+
+print(25032008)
+# input()
+
+width = 5 + 10 + 17
+length = 10
+S = width * length
+print(S)
+
+a = input()
+print(a + ", привет!")
+
+
+
