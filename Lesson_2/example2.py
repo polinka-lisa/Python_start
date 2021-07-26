0e92566ce091ad6f683e532cca45c4cda9b2d42d
polinka-lisa <pmakiyova@ozon.ru>
2021-07-26 15:12:53 +0300

    lesson2

---
+++ Lesson_2/example2.py
@@ -0,0 +1,23 @@
+cars = ['volvo', 'bmw', 'citroen', 'opel', [1, 2, 3]]
+#deleted = cars.pop(0)
+#print(cars, deleted)
+
+#cars[0], cars[1] = cars[1], cars[0]
+del cars[4]
+#cars.remove('citroen')
+cars.sort()
+print(cars)
+cars.reverse()
+print(cars)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lesson_2/example3.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#for elements in numbers:
+#    temp = elements * 10
+#    temp = temp - 5
+#    print(temp)
+
+#print("Цикл выполнен успешно")
+cars = ['volvo', 'bmw', 'citroen', 'opel']
+for i in range(4):
+    print(cars[i], "- элемент под индексом", i, "в списке")
+
+
+
+
+
+
+
